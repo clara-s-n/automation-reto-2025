19871d267b0e3869aebaa7e32ee69ed454a0e613
ElPolisha <lautaro.arrigoni@correo.ucu.edu.uy>
2025-12-18 20:56:20 -0300

    algunos más

--- src/main/java/pages/IngresoCrearFilaPage.java
+++ src/main/java/pages/IngresoCrearFilaPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -14,11 +15,44 @@ public class IngresoCrearFilaPage {
         PageFactory.initElements(driver, this);
     }
 
-    @FindBy(xpath = "(//ion-card)[1]//div")
+    @FindBy(xpath = "//input[@type='email']")
+    private WebElement inputEmail;
+
+    @FindBy(xpath = "//input[@type='password']")
+    private WebElement inputPassword;
+
+    @FindBy(xpath = "/html/body/app-root/ion-app/ion-router-outlet/app-login/ion-content/div/ion-card/ion-card-content/form/ion-button")
+    private WebElement botonLogin;
+
+    @FindBy(xpath = "/html/body/app-root/ion-app/ion-router-outlet/app-tabs/ion-tabs/div/ion-router-outlet/app-ingresos/ion-content/ion-grid/ion-row[2]/ion-col[1]/ion-card")
     private WebElement primerIngreso;
 
-    @FindBy(xpath = "//ion-fab-button")
+    @FindBy(xpath = "/html/body/app-root/ion-app/ion-router-outlet/app-tabs/ion-tabs/div/ion-router-outlet/app-detalle-ingreso/ion-content/ion-fab/ion-fab-button")
     private WebElement botonNuevaFila;
+
+    @FindBy(xpath = "/html/body/app-root/ion-app/ion-router-outlet/app-tabs/ion-tabs/div/ion-router-outlet/app-agregar-fila/ion-content/form/ion-item[1]/ion-input/label/div[2]/input")
+    private WebElement numFila;
+
+    @FindBy(xpath = "/html/body/app-root/ion-app/ion-router-outlet/app-tabs/ion-tabs/div/ion-router-outlet/app-agregar-fila/ion-content/form/ion-item[2]/ion-input/label/div[2]/input")
+    private WebElement descripcion;
+
+    @FindBy(xpath = "//ion-item[4]//ion-input//input")
+    private WebElement precio;
+
+    @FindBy(xpath = "/html/body/app-root/ion-app/ion-router-outlet/app-tabs/ion-tabs/div/ion-router-outlet/app-agregar-fila/ion-content/form/ion-item[3]/div/ion-select")
+    private WebElement selectEmpresa;
+
+    @FindBy(xpath = "//*[@id='ion-overlay-8']//ion-item[1]")
+    private WebElement empresaPrimera;
+
+    @FindBy (xpath = "/html/body/app-root/ion-app/ion-router-outlet/app-tabs/ion-tabs/div/ion-router-outlet/app-agregar-fila/ion-footer/ion-toolbar/ion-grid/ion-row/ion-col[1]/ion-button")
+    private WebElement guardarFila;
+
+    public void hacerLogin(String email, String password) {
+        inputEmail.sendKeys(email);
+        inputPassword.sendKeys(password);
+        botonLogin.click();
+    }
 
     public void abrirPrimerIngreso() {
         primerIngreso.click();
@@ -28,7 +62,21 @@ public class IngresoCrearFilaPage {
         botonNuevaFila.click();
     }
 
-    public boolean terminaEn19() {
-        return driver.getCurrentUrl().endsWith("/19");
+    public void completarFormulario(String fila, String desc, Integer monto) throws InterruptedException {
+        numFila.clear();
+        numFila.sendKeys(fila);
+
+        descripcion.sendKeys(desc);
+        
+        precio.click();
+        precio.sendKeys(monto.toString());
+
+        selectEmpresa.click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//ion-select-popover//ion-item[contains(.,'Baiz')]")).click();
+
+
+
+        guardarFila.click();
     }
 }
