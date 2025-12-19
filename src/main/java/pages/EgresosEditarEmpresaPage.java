9041719cca3631b531b1579c7b2e442714f59b47
Ana Clara Sena <ana.sena@correo.ucu.edu.uy>
2025-12-19 18:48:33 -0300

    Implement SafeClick utility and update page interactions for improved element handling in tests

--- src/main/java/pages/EgresosEditarEmpresaPage.java
+++ src/main/java/pages/EgresosEditarEmpresaPage.java
@@ -4,13 +4,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utils.SafeClick;
+
+import java.time.Duration;
 
 public class EgresosEditarEmpresaPage {
 
     private WebDriver driver;
+    private WebDriverWait wait;
+    private SafeClick safeClick;
 
     public EgresosEditarEmpresaPage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        this.safeClick = new SafeClick(driver, wait);
         PageFactory.initElements(driver, this);
     }
 
@@ -45,17 +54,24 @@ public class EgresosEditarEmpresaPage {
     }
 
     public void editarCI(String ci) throws InterruptedException {
-        tabEmpresas.click();
-        Thread.sleep(4000);
+        wait.until(ExpectedConditions.elementToBeClickable(tabEmpresas));
+        safeClick.safeClick(tabEmpresas);
+        Thread.sleep(2000);
 
-        primeraEmpresa.click();
-        Thread.sleep(4000);
+        wait.until(ExpectedConditions.elementToBeClickable(primeraEmpresa));
+        safeClick.safeClick(primeraEmpresa);
+        Thread.sleep(2000);
 
-        botonEditar.click();
-        Thread.sleep(3000);
+        wait.until(ExpectedConditions.elementToBeClickable(botonEditar));
+        safeClick.safeClick(botonEditar);
+        Thread.sleep(2000);
 
+        wait.until(ExpectedConditions.visibilityOf(inputCI));
         inputCI.clear();
         inputCI.sendKeys(ci);
-        botonGuardar.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(botonGuardar));
+        safeClick.safeClick(botonGuardar);
     }
 }
+
